e94a37772a361b99341360409493fdadc027fc04
masudcodex <forester.masud@gmail.com>
2022-10-25 03:08:40 +0600

    server links have been setup in index.js

--- index.js
+++ index.js
@@ -6,8 +6,31 @@ const port = process.env.port || 5000 ;
 
 app.use(cors());
 
+const categories = require('./data/categories.json')
+const courses = require('./data/courses.json')
+
 app.get('/', (req, res) => {
   res.send('CodeAcademy API running')
+})
+
+app.get('/category', (req, res) => {
+  res.send(categories)
+});
+
+app.get('/category/:id', (req, res) => {
+  const id = req.params.id;
+  const category_course = courses.filter(n => n.category_id === id);
+  res.send(category_course);
+})
+
+app.get('/course', (req, res) =>{
+  res.send(courses);
+});
+
+app.get('/course/:id',(req, res)=>{
+  const id = req.params.id;
+  const selectedCourse = courses.find(n=> n._id === id);
+  res.send(selectedCourse)
 })
 
 app.listen(port, () => {
